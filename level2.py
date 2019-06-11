3a3e771745c5a3b46622cac2f0845a7ebd7c8276
Ngozi Emeh <zingo1ng@yahoo.com>
2019-06-11 20:22:53 +0100

    level 2

--- level2.py
+++ level2.py
@@ -1,2 +1,15 @@
 def two_laptops(ssd, hdd):
-    
+    # Determining if prices are even numbers
+    if ssd % 2 == 0 and hdd % 2 == 0:
+        if ssd > hdd:
+            return hdd
+        else:
+            return ssd
+    else:  
+        if ssd > hdd:
+            return ssd
+        else:
+            return hdd 
+# Calling the function with even and/or odd combination
+two_laptops(300000, 250000)
+two_laptops(300000, 250005) 
